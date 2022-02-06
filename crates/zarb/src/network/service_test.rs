d36c761a06626d234b54e618091c4d66296ade11
Mostafa Sedaghat Joo <mostafa.sedaghat@gmail.com>
2022-02-06 16:37:11 +0800

    Joining general and consensus topic (#14)

--- crates/zarb/src/network/service_test.rs
+++ crates/zarb/src/network/service_test.rs
@@ -6,7 +6,6 @@ mod tests {
     };
     use crate::Service;
     use async_std::task;
-    use libp2p::gossipsub::Topic;
     use simple_logger::SimpleLogger;
     use std::{thread, time::Duration};
 
@@ -34,7 +33,6 @@ mod tests {
         let mut net2 = ZarbNetwork::new(conf2).unwrap();
 
         let net1_sender = net1.message_sender();
-        assert!(net2.register_topic("test".to_owned()).unwrap());
         let net2_receiver = net2.event_receiver();
 
         task::spawn(async {
@@ -49,8 +47,7 @@ mod tests {
         thread::sleep(delay);
 
         let data1 = [1, 2, 3, 4].to_vec();
-        let msg1 = NetworkMessage::PubsubMessage {
-            topic: Topic::new("test"),
+        let msg1 = NetworkMessage::GeneralMessage {
             data: data1.clone(),
         };
 
@@ -61,7 +58,6 @@ mod tests {
             match msg2.unwrap() {
                 NetworkEvent::MessageReceived {
                     source: _,
-                    topic: _,
                     data,
                 } => {
                     assert_eq!(data, data1);
